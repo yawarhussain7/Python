5e02e1b31826c21b0a2d9f8d5a06525359bbe8d5
yawar Hussain <yawarhussain793@gmail.com>
2023-12-23 14:39:46 +0500

    TypeCasting in Python

---
+++ Class_9.py
@@ -0,0 +1,16 @@
+print('Type Casting in Python ')
+
+num = 1
+num2 = 3.4
+
+print("Explicit Type Casting\n When Programmer Explicitly convert one datatype into another datatype  ")
+
+print(num," + ",num2 , " = ",int(num + num2))   # it first solve expression then perform casting
+#in these case we convert float data type into integer type
+
+print("Implicit Type Casting\n When compiler automatically convert ")
+
+print(num," + ",num2, " = ", num + num2)
+# it convert integer type into float type 
+
+
