1fbea65561db67cfbf265b5be9d3fa238d529544
yawar Hussain <yawarhussain793@gmail.com>
2023-12-25 03:02:04 +0500

    String Intro

---
+++ Class_12.py
@@ -0,0 +1,20 @@
+print("String in python ")
+
+name = "Yawar Hussain"
+print(name)
+
+name_1 = 'Hussain'
+print(name_1)
+
+print(name_1[0])
+print(name_1[1])
+print(name_1[2])
+print(name_1[3])
+print(name_1[4])
+print(name_1[5])
+print(name_1[6])
+
+print("Using for loop in Python ")
+for char in name:
+    print(char);
+    
