be7361daada95aa8a8d3378788e36fa99b89070c
yawar Hussain <yawarhussain793@gmail.com>
2023-12-25 02:07:47 +0500

    String Slicing in python

---
+++ Class_11..py
@@ -0,0 +1,16 @@
+print("Strings in python ")
+
+names = "Yawar Husain,Muhammad Hussain"
+
+print(names)
+
+print(len(names))
+
+print("String Slicing ")
+print(names[0:5])
+print(names[5:12])
+print(names[:])
+print(names[-5:29])
+print(names[-12:])
+
+
