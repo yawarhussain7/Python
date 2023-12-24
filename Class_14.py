1ad48250816b34b3644c5fd271e50902665f8217
yawar Hussain <yawarhussain793@gmail.com>
2023-12-25 04:46:59 +0500

    Conditional Operator in Python

---
+++ Class_14.py
@@ -0,0 +1,19 @@
+print("Conditional Operator in python")
+
+marks= int(input("Enter your marks out of 100 : "))
+
+if(marks >= 90):
+    print("You got A+ ")
+
+elif(marks >= 80):
+    print("You got A")
+elif(marks >= 70):
+    print("You got B")
+elif(marks >= 60):
+    print("You got C")
+elif(marks >= 50):
+    print("You got D")
+elif(marks >= 40):
+    print("You got E")
+else:
+    print("Yout got F")
