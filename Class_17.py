051b424dd5a633100dd1ac6a44336feb7c2b9e60
yawar Hussain <yawarhussain793@gmail.com>
2023-12-27 00:49:28 +0500

    For Loop in Python

---
+++ Class_17.py
@@ -0,0 +1,18 @@
+colors = ["Red","Green","Blue","White","Puple","Black"]
+
+
+for color in colors:
+    print(color)
+    for index in color:
+        print(index)
+
+
+print("Operated with numbers ")
+for value in range(0,5):
+    print(value)
+
+
+print("Adding start , pause, and step")
+for index in range(0,20,2):
+    # print(index + 2)  it print 0 to 20 even number
+    print(index )       # it print 0 to 18 even number
