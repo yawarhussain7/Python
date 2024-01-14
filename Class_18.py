718aa399cf500cf2b63c7b835ba5dd08f8f1d5a8
Yawar Hussain <dark@windows.Home>
2024-01-14 06:15:58 -0500

    Introduction to while loop in python

---
+++ Class_18.py
@@ -0,0 +1,31 @@
+# while loop in python 
+# do while loop is not include in python  
+print("This is For Loop ")
+
+for i in range(3):
+    print(i)
+
+
+print("This is While Loop ")
+
+num = 0
+while(num <= 0):
+    num = int(input("Enter the positive number : "))
+    print(i)
+    i +=1
+
+print("Print table using for loop ")
+
+num1 = int(input("Enter the number "))
+
+for index in range(1,11):
+    print(num1," x ",index," = ",index * num1)
+
+print("Print table using While loop ")
+
+i = 0
+
+while(i <= 10):
+    print(num1," x ",i," = ",i * num1)
+    i += 1
+    
