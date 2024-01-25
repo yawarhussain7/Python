c264f0cae9ba13ee3612d72a6a999c37661552fd
Dark Worriror <dark@windows.Home>
2024-01-25 03:08:09 -0500

    Set introduction in python

---
+++ Class_31.py
@@ -0,0 +1,22 @@
+""" 
+Set is the collection of same data type 
+Set avoid the repetative values 
+"""
+
+print("Topic : \n List in python ")
+
+s1 = {3,4,6,2,4,3,5,1,3,5}
+print(type(s1))
+print(s1)
+
+s2 = {"yawar",34,1.2,True}
+print(s2)
+print("Print using for loop ")
+for value in s2:
+    print(value)
+
+# Create a Empty set and check its type and set is empty
+
+s3 = set()
+print(s3)
+print(type(s3))
