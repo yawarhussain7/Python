11c98bf3b6041c98b9fde90ce6245f125c3c21df
Dark Worriror <dark@windows.Home>
2024-01-28 07:18:22 -0500

    Virtual Environment

---
+++ Class_43.py
@@ -0,0 +1,19 @@
+print("Topic : Virtual Environment in Python")
+# install venv 
+print("install Venv \n pip install venv")
+# to create new virtual envirnment 
+print("To create Virtual Environmetn \n python3 -m venv myenv")
+# to activate virtual envirnment 
+print("To Activate in Linux & MacOS  \n source myenv/bin/activate")
+print("To Activate in WIndows \n  myenv/bin/activate.bat")
+print("To Activate in Powershell \n source myenv/bin/activate.ps")
+#to Dactivate virtual environemnt 
+print("To Deactivate  \n Deactivate")
+
+#WHat is  requirement.txt file 
+# it save all the modules with same version install in other computer 
+# Requirement.txt have all the modules with version save in the form of txt
+print("to install requirement.txt \n pip freeze > requirement.txt")
+
+#to install same modules in other computer we use 
+print("pip install -r requirement.txt")
