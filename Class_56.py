1122b0ea18015c7cfa569de1bbf2b7230034eef7
Dark Worriror <dark@windows.Home>
2024-02-07 00:42:14 -0500

    Introduction to OOPs

---
+++ Class_56.py
@@ -0,0 +1,15 @@
+print("Topic: Introduction to OOPs")
+
+# < Class >
+#           A class is the collection of objects
+# Car      
+
+#   < Object >
+#           A Object is the real word entity which have properties and functionalities
+# BMW     Rollrice        Farriy
+
+#   < Encapsulation >
+#       THe main advantage of Encapsulation is that data is hidden and protected from randomly access
+
+#   < Inheritance >
+#   < Polymorephisum >
