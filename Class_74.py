df0dd3752ceef40e91a41718a445d6ccf084d739
Dark Worriror <dark@windows.Home>
2024-02-13 00:20:50 -0500

    Method Overwritting

--- Class_74.py
+++ Class_74.py
@@ -1,22 +1,22 @@
 print("Topic : Method Overwritting ")
-
+#   Parent class
 class Shape:
 
     def __init__(self,x,y):
         self.x = x
         self.y = y
-    
+    # calculating area 
     def area(self):
         return self.x * self.y
 
-
+#   Child Class
 class Circle(Shape):
 
     def __init__(self,radius):
         self.radius = radius
-
+        # accessing Parent Class Constructor
         super().__init__(radius,radius)
-
+        # Calculating area 
     def area(self):
         return 3.14 * super().area()
 
